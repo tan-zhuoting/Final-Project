6c1e0a6c556bcf5817e94a57e26fa96462c2fdc4
Zhuoting Tan <zhuoting@ucla.edu>
2020-05-10 06:21:45 -0700

    Added prompts to show three main functions of this program

--- Final Project.cpp
+++ Final Project.cpp
@@ -1,10 +1,26 @@
 
 
 #include <iostream>
+#include <fstream>
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char choice;
+    std::string input;
+    std::cout << "  Welcome to Geometry analyzer!" << std::endl
+        << "  a. Upload an xyz file for geometry analysis." << std::endl
+        << "  b. Enter molecular formula to check degrees of unsaturation." << std::endl
+        << "  c. Enter IR frequency, check ." << std::endl
+        << "  d. Quit." << std::endl;
+    std::cout << " Do you want to?" << std::endl;
+        std::cin >> choice;
+        std::cout << " Enter your input?";
+        std::cin >> input;
+
+
+
+
 }
 
 
